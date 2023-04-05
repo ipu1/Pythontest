bf8f8e05d4c783a9f70e64258c843813cc56923d
Ilmari <ilmari.muhonen@gmail.com>
2023-04-05 13:36:10 +0300

    testinoora2

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-print("moromorsso")
-print("ei toimi")
+print("SYÖHENKILÖN TIEDOT")
+print("-------------------")
+
+etunimi = input("Anna etunimet: ")
+sukunimi = input("Anna sukunimi: ")
+puhelin = input("Anna puhelinnumero: ")
+sposti = input("Anna sähköpostiosoite: ")
+osoite = input("Anna lähiosoite: ")
+posti = input("Anna postinumero: ")
+kaupunki = input("Anna kaupunki: ")
+maa = input("Anna maa: ")
+vuosi = input("Anna syntymävuosi: ")
+
+print("KIITOS!")
+
+print("HENKILÖN TIEDOT")
+print("----------------")
+
+print("NIMI: ")
+print(")
+print("PUH:")
+
